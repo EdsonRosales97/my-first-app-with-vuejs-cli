9edb06ea4821eb59c0c5d2f279dec528998ff3fa
EdsonRosales97 <erosales@applab.mx>
2021-07-08 14:49:17 -0500

    My first app with Vuejs cli

---
+++ src/components/AccionSaldo.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <button class="btn btn-outline-dark" @click="accion" :disabled="desactivar">
+      {{ texto }}
+  </button>
+
+</template>
+
+<script>
+export default {
+    props: {
+        texto: String,
+        desactivar: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    methods: {
+        accion() {
+            this.$emit('accion');
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
